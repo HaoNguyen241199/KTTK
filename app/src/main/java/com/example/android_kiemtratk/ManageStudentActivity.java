63532d155b1ddcc7b273f793c5db59fc6ec86aaa
6a <haonguyen.241199@gmail.com>
2021-05-28 16:23:29 +0700

    first commit

---
+++ app/src/main/java/com/example/android_kiemtratk/ManageStudentActivity.java
@@ -0,0 +1,131 @@
+package com.example.android_kiemtratk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ManageStudentActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    StudentAdapter studentAdapter;
+    List<Student> list = new ArrayList<>();
+
+    String url = "https://60b0ab351f26610017ffedf4.mockapi.io/students";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_student);
+        recyclerView = findViewById(R.id.rcvStudent);
+
+
+        getData();
+
+
+
+        studentAdapter = new StudentAdapter(list, this);
+        recyclerView.setAdapter(studentAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+
+        EditText edtName = findViewById(R.id.manage_edtName);
+        Button btnAdd = findViewById(R.id.manage_btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString();
+                if(name.length()>0) {
+                    StringRequest stringRequest = new StringRequest(
+                            Request.Method.POST,
+                            url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Toast.makeText(ManageStudentActivity.this, "Create Successfully", Toast.LENGTH_SHORT).show();
+                                    getData();
+                                    edtName.setText("");
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(ManageStudentActivity.this, "Create failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                    ) {
+                        protected Map<String, String> getParams() {
+                            HashMap map = new HashMap();
+                            map.put("name","Hao");
+                            map.put("department","KTPM13");
+                            return map;
+                        }
+                    };
+                    RequestQueue requestQueue = Volley.newRequestQueue(ManageStudentActivity.this);
+                    requestQueue.add(stringRequest);
+                } else {
+                    Toast.makeText(ManageStudentActivity.this, "length > 0", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        });
+
+
+    }
+
+    public void getData() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for(int i=0; i<response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = (JSONObject) response.get(i);
+                                Student student = new Student(
+                                        Integer.parseInt(jsonObject.getString("id")),
+                                        jsonObject.getString("name").toString(),
+                                        jsonObject.getString("department").toString()
+                                        );
+                                list.add(student);
+                                studentAdapter = new StudentAdapter(list, ManageStudentActivity.this);
+                                recyclerView.setAdapter(studentAdapter);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+}
---
+++ app/src/main/java/com/example/android_kiemtratk/StudentAdapter.java
@@ -0,0 +1,148 @@
+package com.example.android_kiemtratk;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+
+    List<Student> list = new ArrayList<>();
+    Context context;
+    LayoutInflater inflater;
+
+    String url = "https://60ae49ae80a61f0017332eed.mockapi.io/students";
+
+    public StudentAdapter(List<Student> list, Context context) {
+        this.list = list;
+        this.context = context;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public StudentAdapter.StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = inflater.inflate(R.layout.list_item, parent, false);
+        return new StudentViewHolder(v, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentAdapter.StudentViewHolder holder, int position) {
+        Student student = list.get(position);
+
+        holder.tvName.setText(student.getName());
+        holder.tvDepartment.setText(student.getDepartment());
+
+        holder.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest stringRequest = new StringRequest(
+                        Request.Method.PUT,
+                        url + "/" + student.getId(),
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(context, "Successfully", Toast.LENGTH_SHORT).show();
+                                list.clear();
+                                getData();
+
+                                notifyDataSetChanged();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                ){
+                    protected Map<String, String> getParams() {
+                        HashMap map = new HashMap();
+                        map.put("name","NguyenTrongHao");
+                        map.put("department","KTPM13");
+                        return map;
+                    }
+                };
+                RequestQueue requestQueue = Volley.newRequestQueue(context);
+                requestQueue.add(stringRequest);
+
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class StudentViewHolder extends RecyclerView.ViewHolder {
+        StudentAdapter studentAdapter;
+        TextView tvName, tvDepartment;
+        ImageButton imageButton;
+
+        public StudentViewHolder(@NonNull View itemView, StudentAdapter studentAdapter) {
+            super(itemView);
+            this.tvName = itemView.findViewById(R.id.tv_name);
+            this.tvDepartment = itemView.findViewById(R.id.tvDepartment);
+            this.imageButton = itemView.findViewById(R.id.imgbUpdate);
+            this.studentAdapter = studentAdapter;
+        }
+    }
+
+    public void getData() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for(int i=0; i<response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = (JSONObject) response.get(i);
+                                Student student = new Student(
+                                        Integer.parseInt(jsonObject.getString("id")),
+                                        jsonObject.getString("name").toString(),
+                                        jsonObject.getString("department").toString()
+                                );
+                                list.add(student);
+                                notifyDataSetChanged();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(jsonArrayRequest);
+    }
+}
